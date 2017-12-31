54475ccc9c03e913d8d6a9fdb734323bf3e8f4c1
Douglas W Schultz <vxpx17@gmail.com>
2017-12-31 20:58:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -71,10 +71,10 @@ function placeOrder(cardNumber) {
 if (typeof(cardNumber)!=='number'){
   console.log('Sorry, we don\'t have a credit card on file for you.');return;}
   else {
-    let orderMessage=`Your total cost is $${total()}, which will be charged to the card;
-
-}
-  
+    let orderMessage=`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
+    console.log(orderMessage);
+    cart=[];
+    }
 }
 
 console.log(findPrice());
